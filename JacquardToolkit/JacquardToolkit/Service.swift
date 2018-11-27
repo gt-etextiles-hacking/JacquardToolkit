a49cc4b49d48d6f4b0b7cc002c000fd58818b9de
calebrudnicki <calebrudnicki@gmail.com>
2018-11-27 09:40:38 -0500

    First commit

---
+++ JacquardToolkit/JacquardToolkit/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  JacquardToolkit
+//
+//  Created by Caleb Rudnicki on 11/27/18.
+//  Copyright © 2018 Caleb Rudnicki. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did Some Stuff, here you go homeboi"
+    }
+    
+}
