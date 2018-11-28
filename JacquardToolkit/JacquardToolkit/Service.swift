6adb230964f6a9c0287d89435b8abf3e0b577e59
calebrudnicki <calebrudnicki@gmail.com>
2018-11-28 09:31:28 -0500

    Added method to connect to jacket

--- JacquardToolkit/JacquardToolkit/Service.swift
+++ JacquardToolkit/JacquardToolkit/Service.swift
@@ -7,18 +7,66 @@
 //
 
 import Foundation
+import CoreBluetooth
 
-public class Service {
+protocol ServiceDelegate {
+    func getValue() -> String
+}
+
+open class Service: UIViewController, ServiceDelegate {
     
-    private init() {}
+    func getValue() -> String {
+        return "GOT VALUE"
+    }
+
+    public var centralManager: CBCentralManager!
+    private var peripheralObject: CBPeripheral!
+    private var peripheralList: [CBPeripheral] = []
+    private let uuid = UUID(uuidString: "3DF4C660-AAE3-FC91-DBE5-0217FCDE7894")
+    private var jacketIsConnected = false
+    
+//    private init() {}
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     public static func doSomething() -> String {
         return "Did Some Stuff, here you go homeboi"
     }
-    
-    public static func connectToJacket() -> String {
-        return "CONNECTING TO JACKET"
+
+    public func connectToJacket() -> Bool {
+        if jacketIsConnected {
+            centralManager.cancelPeripheralConnection(peripheralObject)
+            return false
+        }
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        return true
     }
+
+}
+
+extension Service: CBCentralManagerDelegate {
     
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("Unknown")
+        case .resetting:
+            print("Resetting")
+        case .unsupported:
+            print("Unsupported")
+        case .unauthorized:
+            print("Unauthorized")
+        case .poweredOn:
+            print("Powered On")
+            peripheralList = centralManager.retrievePeripherals(withIdentifiers: [uuid!])
+            peripheralObject = peripheralList[0]
+            peripheralObject.delegate = self as! CBPeripheralDelegate
+            centralManager.connect(peripheralObject, options: nil)
+        case .poweredOff:
+            print("Powered Off")
+        }
+    }
     
 }
