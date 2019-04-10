4a7097669f429918ef63699a13250213c4f32449
aayush <aayush.kumarmail@gmail.com>
2019-04-10 19:54:52 -0400

    fixed threads that were hooked up incorrectly to UI and consolidated them into a threads array

--- JacquardToolkitExample/JacquardToolkitExample/ViewController.swift
+++ JacquardToolkitExample/JacquardToolkitExample/ViewController.swift
@@ -14,22 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var connectionIndicator: UIImageView!
     @IBOutlet weak var lastGestureLabel: UILabel!
     
-    @IBOutlet weak var thread1: UIImageView!
-    @IBOutlet weak var thread2: UIImageView!
-    @IBOutlet weak var thread3: UIImageView!
-    @IBOutlet weak var thread4: UIImageView!
-    @IBOutlet weak var thread5: UIImageView!
-    @IBOutlet weak var thread6: UIImageView!
-    @IBOutlet weak var thread7: UIImageView!
-    @IBOutlet weak var thread8: UIImageView!
-    @IBOutlet weak var thread9: UIImageView!
-    @IBOutlet weak var thread10: UIImageView!
-    @IBOutlet weak var thread11: UIImageView!
-    @IBOutlet weak var thread12: UIImageView!
-    @IBOutlet weak var thread13: UIImageView!
-    @IBOutlet weak var thread14: UIImageView!
-    @IBOutlet weak var thread15: UIImageView!
-    
+    @IBOutlet var threads:[UIImageView]!
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -84,22 +69,9 @@ extension ViewController: JacquardServiceDelegate {
     }
     
     func didDetectThreadTouch(threadArray: [Float]) {
-//        print("Threads: \(threadArray)")
-        thread1.alpha = CGFloat(threadArray[0])
-        thread2.alpha = CGFloat(threadArray[1])
-        thread3.alpha = CGFloat(threadArray[2])
-        thread4.alpha = CGFloat(threadArray[3])
-        thread5.alpha = CGFloat(threadArray[4])
-        thread6.alpha = CGFloat(threadArray[5])
-        thread7.alpha = CGFloat(threadArray[6])
-        thread8.alpha = CGFloat(threadArray[7])
-        thread9.alpha = CGFloat(threadArray[8])
-        thread10.alpha = CGFloat(threadArray[9])
-        thread11.alpha = CGFloat(threadArray[10])
-        thread12.alpha = CGFloat(threadArray[11])
-        thread13.alpha = CGFloat(threadArray[12])
-        thread14.alpha = CGFloat(threadArray[13])
-        thread15.alpha = CGFloat(threadArray[14])
+        for (index, thread) in threads.enumerated() {
+            thread.alpha = CGFloat(threadArray[index])
+        }
     }
     
     func didDetectForceTouchGesture() {
