9d2f8a045866db75db9c42aa297aa99486ad35ce
aayush <aayush.kumarmail@gmail.com>
2019-04-13 20:52:45 -0400

    cleaned up UI hooks/outlets, added logging into toolkit, and basic 20-line email functionality into ToolkitExample to actually export logs, and added basic icon

--- JacquardToolkitExample/JacquardToolkitExample/ViewController.swift
+++ JacquardToolkitExample/JacquardToolkitExample/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import MessageUI
 import JacquardToolkit
 
 class ViewController: UIViewController {
-    
+    // UI Outlets
     @IBOutlet weak var connectionIndicator: UIImageView!
     @IBOutlet weak var lastGestureLabel: UILabel!
-    
+    @IBOutlet weak var loggingButton: UIButton!
+    @IBOutlet weak var rainbowGlowButton: UIButton!
     @IBOutlet var threads:[UIImageView]!
+    
+    // CSV Logging Constants
+    private let fileName = "data.csv"
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +27,15 @@ class ViewController: UIViewController {
         for thread in threads {
             thread.alpha = CGFloat(0.05)
         }
+        updateUI(isConnected: false)
+    }
+    
+    public func updateUI(isConnected: Bool) {
+        connectionIndicator.image = UIImage(named: isConnected ? "GreenCircle" : "RedCircle")
+        rainbowGlowButton.isEnabled = isConnected
+        rainbowGlowButton.alpha = CGFloat(isConnected ? 1 : 0.4)
+        loggingButton.isEnabled = isConnected
+        loggingButton.alpha = CGFloat(isConnected ? 1 : 0.7)
     }
     
     @IBAction func connectButtonTapped(_ sender: Any) {
@@ -34,16 +48,27 @@ class ViewController: UIViewController {
         JacquardService.shared.rainbowGlowJacket()
     }
     
+    @IBAction func loggingButtonToggled(_ sender: Any) {
+        if JacquardService.shared.loggingThreads {
+            let csvText = JacquardService.shared.exportLog()
+            let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
+            
+            do {
+                try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+                self.sendMail(dataURL: path!)
+            } catch {
+                print("Failed to create/send csv file: \(error)")
+            }
+        }
+        loggingButton.setTitle(JacquardService.shared.loggingThreads ? "Start Logging" : "Stop Logging", for: UIControl.State.normal)
+        JacquardService.shared.loggingThreads = !JacquardService.shared.loggingThreads
+    }
 }
 
 extension ViewController: JacquardServiceDelegate {
     
     func didDetectConnection(isConnected: Bool) {
-        if isConnected {
-            connectionIndicator.image = UIImage(named: "GreenCircle")
-        } else {
-            connectionIndicator.image = UIImage(named: "RedCircle")
-        }
+        self.updateUI(isConnected: isConnected)
     }
     
     func didDetectDoubleTapGesture() {
@@ -81,4 +106,35 @@ extension ViewController: JacquardServiceDelegate {
         print("didDetectForceTouchGesture")
         lastGestureLabel.text = "Force Touch"
     }
+    
+}
+
+// Extension class for emailing log files
+extension ViewController: MFMailComposeViewControllerDelegate {
+    
+    func sendMail(dataURL: URL) {
+        if( MFMailComposeViewController.canSendMail()) {
+            // attach logged csv data to email and display compose pop-up
+            let mailComposerVC = MFMailComposeViewController()
+            mailComposerVC.mailComposeDelegate = self
+            mailComposerVC.setSubject("Thread Pressure Readings: \(NSDate().description)")
+            do {
+                try mailComposerVC.addAttachmentData(NSData(contentsOf: dataURL, options: NSData.ReadingOptions.mappedRead) as Data, mimeType: "text/csv", fileName: fileName)
+            } catch {
+                print("Couldn't Attach \(fileName)")
+            }
+            self.present(mailComposerVC, animated: true, completion: nil)
+        } else {
+            // email failed to send
+            let sendMailErrorAlert = UIAlertController(title: "Could not send email", message: "Your device could not send email", preferredStyle: .alert)
+            let dismiss = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            sendMailErrorAlert.addAction(dismiss)
+            self.present(sendMailErrorAlert, animated: true, completion: nil)
+        }
+    }
+    
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
 }
