f2965ef7c25558ebc04ed698aac5ec8e07deef3c
calebrudnicki <calebrudnicki@gmail.com>
2019-04-21 00:13:01 -0400

    Updated the QR code scanner view

--- JacquardToolkit/JSQRCodeScannerView.swift
+++ JacquardToolkit/JSQRCodeScannerView.swift
@@ -12,9 +12,16 @@ public class JSQRCodeScannerView: UIView {
 
     private var video = AVCaptureVideoPreviewLayer()
     private var session = AVCaptureSession()
+    private var scannerBounds = CAShapeLayer()
+    private var textLayer = CATextLayer()
+    private let output = AVCaptureMetadataOutput()
     
+    private var scannerRect = CGRect()
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        scannerRect = CGRect(x: self.center.x - (self.frame.width * 0.667 / 2), y: self.frame.width * 0.667 / 4, width: self.frame.width * 0.667, height: self.frame.width * 0.667)
         
         if let captureDevice = AVCaptureDevice.default(for: .video) {
             do {
@@ -24,7 +31,6 @@ public class JSQRCodeScannerView: UIView {
                 print("Error")
             }
             
-            let output = AVCaptureMetadataOutput()
             session.addOutput(output)
             
             output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
@@ -32,7 +38,19 @@ public class JSQRCodeScannerView: UIView {
             
             video = AVCaptureVideoPreviewLayer(session: session)
             video.frame = self.layer.bounds
+            
+            scannerBounds.frame = scannerRect
+            scannerBounds.borderColor = UIColor.yellow.cgColor
+            scannerBounds.borderWidth = 5
+            
+            textLayer.string = "Scan QR Code"
+            textLayer.alignmentMode = .center
+            textLayer.backgroundColor = UIColor.yellow.cgColor
+            textLayer.frame = CGRect(x: 15, y: self.frame.height + 15 - (self.frame.height * 0.25), width: self.frame.width - 30, height: self.frame.height * 0.25 - 30)
+            
             self.layer.addSublayer(video)
+            self.layer.insertSublayer(scannerBounds, above: video)
+            self.layer.insertSublayer(textLayer, above: video)
         }
     }
     
@@ -41,7 +59,9 @@ public class JSQRCodeScannerView: UIView {
     }
     
     public func startScanner() {
+        session.commitConfiguration()
         session.startRunning()
+        output.rectOfInterest = video.metadataOutputRectConverted(fromLayerRect: scannerRect)
     }
     
     public func stopScanner() {
@@ -54,10 +74,11 @@ public class JSQRCodeScannerView: UIView {
 extension JSQRCodeScannerView: AVCaptureMetadataOutputObjectsDelegate {
     
     public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        textLayer.string = "Pop tag in"
+        session.stopRunning()
         if let object = metadataObjects.first as? AVMetadataMachineReadableCodeObject {
             if object.type == .qr {
                 JacquardService.shared.updateJacketIDString(jacketIDString: object.stringValue)
-                session.startRunning()
             }
         }
     }
