2c0e998089718d4e5f9a8c7ab7a52bb55d61fb64
Caleb Rudnicki <calebrudnicki@Calebs-MacBook-Pro.local>
2019-09-22 10:27:14 -0400

    Fixed a boolean for the PR

--- JacquardToolkitExample/JacquardToolkitExample/ViewController.swift
+++ JacquardToolkitExample/JacquardToolkitExample/ViewController.swift
@@ -120,7 +120,7 @@ extension ViewController: JacquardServiceDelegate {
         connectButton.title = isConnected ? "Connected" : "Connect"
         connectButton.isEnabled = !isConnected
         rainbowGlowButton.isEnabled = isConnected
-        playTutorialButton.isEnabled = true//isConnected
+        playTutorialButton.isEnabled = isConnected
         for thread in threadArray {
             thread.alpha = isConnected ? 1.0 : 0.3
         }
